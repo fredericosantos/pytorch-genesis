a8a4c8f17051b077048d0b246e803d02e49c93b2
Frederico Santos <1119791+fredericosantos@users.noreply.github.com>
2022-09-15 17:10:27 +0100

    Initialize update-setup

---
+++ project/__init__.py
@@ -0,0 +1,25 @@
+"""Root package info."""
+
+# TODO: define initial version
+__version__ = '0.0.0'
+# TODO: set your name nad your colleagues
+__author__ = 'PyTorch Lightning et al.'
+# TODO: add contact mail address
+__author_email__ = 'name@pytorchlightning.ai'
+# TODO: define licensing of your work, e.g. MIT, Apache 2.0
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c) 2020-2020, %s.' % __author__
+# TODO: edit this page link
+__homepage__ = 'https://github.com/PyTorchLightning/pytorch-lightning-conference-seed'
+# TODO: add one-line description
+__docs__ = "Research Seed."
+# TODO: add multi-line description
+__long_doc__ = """
+What is it?
+-----------
+This is starter project template which shall simplify initial steps for each new PL project...
+
+Except the implemented sections:
+ - sample package
+ - setting CI
+"""
